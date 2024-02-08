13a47c8d3cb95b99fd83197b146dc9a9a0073f57
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-08 15:12:50 +0530

    Create transmissionTest.js

---
+++ tests/transmissionTest.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const { transmissionFailureCount } = require("./transmitter.js");
+function transmissonTest() {
+  expect(transmissionFailureCount).to.equal(0);
+  expect(transmissionFailureCount).to.equal(2);
+  expect(transmissionFailureCount).to.equal(5);
+  expect(transmissionFailureCount).to.equal(10);
+}
+transmissonTest();
