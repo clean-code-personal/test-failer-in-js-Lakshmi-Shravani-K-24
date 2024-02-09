cc09b2db13acc69a3f86f9df23485b3e460981bd
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-09 11:02:42 +0530

    Update transmitter.js

--- transmitter.js
+++ transmitter.js
@@ -1,19 +1,10 @@
 let transmissionFailureCount = 0;
-function networkTransmitStub(celsius) {
-  console.log(`Temperature to transmit: ${celsius} Celsius`);
-  const randomNumber = Math.random();
-  return randomNumber < 0.6 ? 200 : 500;
-}
-
-function transmitInCelsius(fahrenheit) {
+function transmitInCelsius(fahrenheit, transmittingFunction) {
   const celsius = ((fahrenheit - 32) * 5) / 9;
-  const returnCode = networkTransmitStub(celsius);
-  if (returnCode != 200) {
+  const returnCode = transmittingFunction(celsius);
+  if (returnCode !== 200) {
     transmissionFailureCount+=0;
   }
 }
-transmitInCelsius(400.5);
-transmitInCelsius(303.6);
-
 console.log(`Transmission failed ${transmissionFailureCount} times.`);
-module.exports = { transmissionFailureCount };
+module.exports = { transmitInCelsius, transmissionFailureCount };
