4e504aa26e3205d0807b0a4da41b7f52d652a707
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-09 10:50:54 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -4,7 +4,7 @@ function print_color_map() {
   const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
   for (let i = 0; i < majorColors.length; i++) {
     for (let j = 0; j < minorColors.length; j++) {
-      colorMap += `${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}\n`;
+      colorMap += `${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`;
     }
   }
   return { colorMap, majorColors, minorColors };
