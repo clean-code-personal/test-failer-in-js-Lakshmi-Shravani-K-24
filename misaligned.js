357faae4e10ae2ea876b96af3423e44262e5dacf
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-12 08:54:07 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -1,21 +1,24 @@
 function print_color_map() {
-let colorMap = "";
-const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-//Heading
-colorMap += `Pair Number | Major Color | Minor Color\n`;
-colorMap += `---------------------------------------\n`;
+  let heading = "";
+  let colorMap = "";
 
-// Iterate over major and minor colors to generate the color map
-for (let i = 0; i < majorColors.length; i++) {
-  for (let j = 0; j < minorColors.length; j++) {
-    const index = i * 5 + j + 1;
-    const majorColor = majorColors[i];
-    const minorColor = minorColors[j];
-    colorMap += `${' '.repeat(5)}${index.toString().padEnd(6)} | ${majorColor.padEnd(11)} | ${minorColor}\n`;
+  const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+  const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+  //Heading
+  heading += `Pair Number | Major Color | Minor Color\n`;
+  heading += `---------------------------------------`;
+
+  // Iterate over major and minor colors to generate the color map
+  for (let i = 0; i < majorColors.length; i++) {
+    for (let j = 0; j < minorColors.length; j++) {
+      const index = i * 5 + j + 1;
+      const majorColor = majorColors[i];
+      const minorColor = minorColors[j];
+      colorMap += `${" ".repeat(5)}${index
+        .toString()
+        .padEnd(6)} | ${majorColor.padEnd(11)} | ${minorColor}\n`;
+    }
   }
+  return { colorMap, heading, majorColors, minorColors };
 }
- return { colorMap, majorColors, minorColors };
-}
-module.exports = { print_color_map };
-
+module.exports={print_color_map};
