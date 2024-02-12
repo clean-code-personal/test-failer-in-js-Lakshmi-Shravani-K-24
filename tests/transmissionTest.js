0ed80a6d7f34692917c0cff5902cc869e0866eee
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-12 09:18:05 +0530

    Update transmissionTest.js

--- tests/transmissionTest.js
+++ tests/transmissionTest.js
@@ -1,21 +1,24 @@
 const { expect } = require("chai");
-const { transmitInCelsius, transmissionFailureCount } = require("../transmitter.js");
-
+const {
+  transmitInCelsius,
+  transmissionFailureCount,
+} = require("../transmitter.js");
 // Stub function for network transmission
-function networkTransmitStub(celsius) {
-  console.log(`Temperature to transmit: ${celsius} Celsius`);
-  const randomNumber = Math.random();
-  return randomNumber < 0.6 ? 200 : 500;
+function networkTransmitStubSuccess(celsius) {
+  console.log(`Temperature  transmitted: ${celsius} Celsius`);
+  return 200;
+}
+function networkTransmitStubFailure(celsius) {
+  console.log(`Cannot transmit temperature : ${celsius} Celsius`);
+  return 500;
 }
 function transmissonTest() {
-   // Injecting the stub function when calling transmitInCelsius
-  transmitInCelsius(400.5, networkTransmitStub);
-  transmitInCelsius(303.6, networkTransmitStub);
-  transmitInCelsius(504.7, networkTransmitStub);
-  expect(transmissionFailureCount).to.equal(0);//passes
-  expect(transmissionFailureCount).to.equal(2);//fails
-  expect(transmissionFailureCount).to.equal(5);// fails
-  expect(transmissionFailureCount).to.equal(10);//fails
+  // Injecting the stub function when calling transmitInCelsius
+  transmitInCelsius(400.5, networkTransmitStubSuccess);
+  transmitInCelsius(303.6, networkTransmitStubSuccess);
+  // transmitInCelsius(504.7, networkTransmitStubFailure);
+  console.log(`Transmission failed ${transmissionFailureCount} times.`);
+  expect(transmissionFailureCount).to.equal(0); //passes
+  // expect(transmissionFailureCount).to.equal(1); //passes if we use failre stub once
 }
 transmissonTest();
-
