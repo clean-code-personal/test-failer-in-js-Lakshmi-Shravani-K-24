9267d942685a333cf4e1e844e4611e7a60e37d5c
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-12 13:36:29 +0530

    Update transmissionTest.js

--- tests/transmissionTest.js
+++ tests/transmissionTest.js
@@ -16,9 +16,7 @@ function transmissonTest() {
   // Injecting the stub function when calling transmitInCelsius
   transmitInCelsius(400.5, networkTransmitStubSuccess);
   transmitInCelsius(303.6, networkTransmitStubSuccess);
-  // transmitInCelsius(504.7, networkTransmitStubFailure);
   console.log(`Transmission failed ${transmissionFailureCount} times.`);
   expect(transmissionFailureCount).to.equal(0); //passes
-  // expect(transmissionFailureCount).to.equal(1); //passes if we use failre stub once
 }
 transmissonTest();
