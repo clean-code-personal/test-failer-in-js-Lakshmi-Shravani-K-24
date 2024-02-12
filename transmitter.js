c5c1efec03af20addc8e97812d93970384d71fcc
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-12 09:18:42 +0530

    Update transmitter.js

--- transmitter.js
+++ transmitter.js
@@ -3,8 +3,7 @@ function transmitInCelsius(fahrenheit, transmittingFunction) {
   const celsius = ((fahrenheit - 32) * 5) / 9;
   const returnCode = transmittingFunction(celsius);
   if (returnCode !== 200) {
-    transmissionFailureCount+=0;
+    transmissionFailureCount+=1;
   }
 }
-console.log(`Transmission failed ${transmissionFailureCount} times.`);
 module.exports = { transmitInCelsius, transmissionFailureCount };
